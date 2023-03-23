68bd44abfaff91f8e03b32040b0c208d926218f1
Zaki Nadhif <zaki.nadhif45@gmail.com>
2023-03-23 21:49:19 +0700

    Write datastore IPC channel boilerplate

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,8 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+const { contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld('datastoreApi', {
+
+});
