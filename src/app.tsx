8a7cfbf92f97dd22ca7fc1c9fdc65c5fc542a28a
Zaki Nadhif <zaki.nadhif45@gmail.com>
2023-03-23 11:45:43 +0700

    Add `react-router-dom` support

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,21 @@
-import * as ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
-}
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-render();
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
